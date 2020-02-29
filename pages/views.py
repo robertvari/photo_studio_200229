821f217456238da3e2efed508246b6b3da19d501
Robert Vari <mail.robertvari@gmail.com>
2020-02-29 12:15:07 +0100

    html templates: _base.html _menu.html

--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1> Hello World! This is my new Django project </h1>")
+    return render(request, 'home.html')
 
 
 def gallery(request):
-    return HttpResponse("<h1>Gallery Page</h1>")
+    return render(request, 'gallery.html')
 
 
 def contact(request):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, 'contact.html')
