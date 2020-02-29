7c28e3abd590287df51987f70731235faa09719a
Robert Vari <mail.robertvari@gmail.com>
2020-02-29 11:21:54 +0100

    Testing django urls

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1> Hello World! This is my new Django project </h1>")
+
+
+def gallery(request):
+    return HttpResponse("<h1>Gallery Page</h1>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Contact Page</h1>")
