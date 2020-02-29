50d6889e66a9429121373cb24df34f30ade2d939
Robert Vari <mail.robertvari@gmail.com>
2020-02-29 12:28:36 +0100

    Using url names

--- pages/urls.py
+++ pages/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import home, contact, gallery, about
 
 urlpatterns = [
-    path('', home),
-    path('gallery/', gallery),
-    path('contact/', contact),
-    path('about/', about),
+    path('', home, name="home"),
+    path('gallery/', gallery, name="gallery"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about"),
 ]
