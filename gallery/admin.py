177851d6b27846fa42b96ed5298d8323de8f419c
Robert Vari <mail.robertvari@gmail.com>
2020-03-22 10:47:54 +0100

    Photo model and handling media

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Photo
+
 admin.site.register(Category)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ["title", "uploaded", "image"]
+    search_fields = ["title", "comment"]
+
+admin.site.register(Photo, PhotoAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_delete
+import os
 
 
 class Category(models.Model):
@@ -6,3 +8,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    image = models.ImageField(upload_to="media")
+    comment = models.TextField()
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+def photo_cleanup(sender, instance, **kwargs):
+    os.remove(instance.image.path)
+
+
+post_delete.connect(photo_cleanup, sender=Photo)
